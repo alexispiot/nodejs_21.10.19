aecbf9babc103e32294eaaf472376f6913883476
atacanjacques <atacan.jacques@gmail.com>
2021-10-19 15:53:15 +0200

    Node server + nodemon

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname ='0.0.0.0';
+const port = 3000;
+
+const server = http.createServer(
+    (req, res) => {
+
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain');
+        res.end('Hello world');
+
+    }
+);
+
+server.listen(port, hostname);
