44d6028069c69eeb9368c8104c48e291309c5d13
atacanjacques <atacan.jacques@gmail.com>
2021-10-28 12:22:53 +0200

    Add a mongoose CRUD

--- src/app.js
+++ src/app.js
@@ -5,57 +5,13 @@ const port = 3000;
 
 const server = express();
 
-server.get("/", (req, res) => {
-    res.status(200);
-    res.type("html");
-    res.end("Accueil");
-})
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://mongo/apib2');
 
-server.get("/posts", (req, res) => {
-    res.status(200);
-    res.type("html");
-    res.end("Liste des articles");
-})
+server.use(express.urlencoded());
+server.use(express.json());
 
-
-server.get("/posts/:post_id", (req, res) => {
-    res.status(200);
-    res.type("html");
-    res.end(`Article : ${req.params.post_id}`);
-})
-
-server.get("/users", (req, res) => {
-    res.status(200);
-    res.type("html");
-    res.end("Liste des utilisateurs");
-})
-
-server.get("/users/:profil_name", (req, res) => {
-    res.status(200);
-    res.type("html");
-    res.end(`Bonjour ${req.params.profil_name}`);
-})
-
-// const server = http.createServer(
-//     (req, res) => {
-
-//         // let url = req.url;
-//         let {url} = req;
-
-//         console.log(req.url);
-
-//         if(url == "/posts"){
-//             res.statusCode = 200;
-//             res.setHeader('Content-type', 'text/plain');
-//             res.end('Liste des articles');
-//         }
-//         else if(url == "/users"){
-//             res.statusCode = 200;
-//             res.setHeader('Content-type', 'text/plain');
-//             res.end('Liste des utilisateurs');
-//         }
-
-//     }
-// );
+const postRoute = require("./routes/postRoute");
+postRoute(server);
 
 server.listen(port, hostname);
