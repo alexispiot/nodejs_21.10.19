f261e63b1e03fc69819309c36e7eea1bcbddcf71
atacanjacques <atacan.jacques@gmail.com>
2021-10-28 09:55:49 +0200

    Add express

--- src/app.js
+++ src/app.js
@@ -1,28 +1,61 @@
-const http = require('http');
+const express = require('express');
 
 const hostname ='0.0.0.0';
 const port = 3000;
 
-const server = http.createServer(
-    (req, res) => {
+const server = express();
 
-        // let url = req.url;
-        let {url} = req;
+server.get("/", (req, res) => {
+    res.status(200);
+    res.type("html");
+    res.end("Accueil");
+})
 
-        console.log(req.url);
+server.get("/posts", (req, res) => {
+    res.status(200);
+    res.type("html");
+    res.end("Liste des articles");
+})
 
-        if(url == "/posts"){
-            res.statusCode = 200;
-            res.setHeader('Content-type', 'text/plain');
-            res.end('Liste des articles');
-        }
-        else if(url == "/users"){
-            res.statusCode = 200;
-            res.setHeader('Content-type', 'text/plain');
-            res.end('Liste des utilisateurs');
-        }
 
-    }
-);
+server.get("/posts/:post_id", (req, res) => {
+    res.status(200);
+    res.type("html");
+    res.end(`Article : ${req.params.post_id}`);
+})
+
+server.get("/users", (req, res) => {
+    res.status(200);
+    res.type("html");
+    res.end("Liste des utilisateurs");
+})
+
+server.get("/users/:profil_name", (req, res) => {
+    res.status(200);
+    res.type("html");
+    res.end(`Bonjour ${req.params.profil_name}`);
+})
+
+// const server = http.createServer(
+//     (req, res) => {
+
+//         // let url = req.url;
+//         let {url} = req;
+
+//         console.log(req.url);
+
+//         if(url == "/posts"){
+//             res.statusCode = 200;
+//             res.setHeader('Content-type', 'text/plain');
+//             res.end('Liste des articles');
+//         }
+//         else if(url == "/users"){
+//             res.statusCode = 200;
+//             res.setHeader('Content-type', 'text/plain');
+//             res.end('Liste des utilisateurs');
+//         }
+
+//     }
+// );
 
 server.listen(port, hostname);
